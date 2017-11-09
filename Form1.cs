384d061367ad1e985f32ebc29318ce78015b3b6d
Lucas Corssac <lcorssac@hotmail.com>
2017-11-09 08:28:57 -0200

    Added a shitfest that doesn't work

--- Form1.cs
+++ Form1.cs
@@ -12,36 +12,50 @@ namespace fpi_trab
 {
     public partial class Form1 : Form
     {
+        private PictureBox[] picboxes = new PictureBox[2];
+        private int picBoxIndex = 0;
+        private int placeW = 0, placeH = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private Image picture_loader(string picture_name)
+        private Image pictureLoader(string pictureName)
         {
-            Image image = Image.FromFile(picture_name);
+            Image image = Image.FromFile(pictureName);
+
             var picture = new PictureBox
             {
                 Name = "pictureBox",
-                Size = new Size(16, 16),
-                Location = new Point(100, 100),
+                Size = new Size(image.Width, image.Height),
+                Location = new Point(placeW, placeH),
                 Image = image,
 
             };
+
+            placeW += image.Width;
+            placeH += image.Height;
+            picBoxIndex += 1;
+
+            picboxes[picBoxIndex] = picture;
+
             this.Controls.Add(picture);
+            
             return image;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Image image = picture_loader("spurdo.jpg");
+            Image image = pictureLoader("spurdo.jpg");
+            pictureLoader("johnny.jpg");
             // Set the PictureBox image property to this image.
-            this.Controls.   pictureBox.Image = image;
-            pictureBox.Height = image.Height;
-            pictureBox.Width = image.Width;
+            //this.Controls.pictureBox.Image = image;
+            //pictureBox.Height = image.Height;
+            //pictureBox.Width = image.Width;
 
-            this.Height = image.Height + 100;
-            this.Width = image.Width + 100;
+            this.Height = image.Height;
+            this.Width = image.Width;
 
             image.Save("johnny.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 
